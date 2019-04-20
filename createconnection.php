147309b6e6c1186e36382fb51d80fb4cc2ff9933
Leni Lunderman <lenilunderman@Lenis-Air.fios-router.home>
2019-04-20 18:02:05 -0400

    code is working! yay!

--- createconnection.php
+++ createconnection.php
@@ -2,33 +2,18 @@
 // Create Connection to MySQL Database
 //mysqli_connect("hostname", "username", "password", "database");
 
-//connect to the teacher server
-include "../../../connection_string.php";
-if (mysql_query("CREATE DATABASE myclue", $con)) {
-    echo "database myclue created!";
-} else {
-    echo mysql_error();
-}
-mysql_select_db("myclue", $con);
-$sql1 = "CREATE TABLE 'login' (
-  'ID' int(11) NOT NULL,
-  'userName' varchar(255) NOT NULL,
-  'userPassword' varchar(255) DEFAULT NULL,
-  'userEmail' varchar(255) DEFAULT NULL
-)  CHARSET=utf8";
-$sql2 = "CREATE TABLE 'notes' (
-  'ID_notes' int(11) NOT NULL,
-  'User_ID' int(11) NOT NULL,
-  'title_note' varchar(255) DEFAULT NULL,
-  'type_note' varchar(80) DEFAULT NULL,
-  'note' text
-) CHARSET=utf8;";
+$con = mysqli_connect("localhost", "leni", "root", "myclue");
 
-if (mysql_query($sql, $con)) {
-        echo "table profTest3272019table created!";
-    } else {
-        echo mysql_error();
-    }
-mysql_close($con);
+// Check the connection
+if ($con === FALSE){
+    die("ERROR to connect to mysql: " . mysqli_connect_error());
+}
+else{
+    echo "Connected Succefully! " . mysqli_get_host_info($con); 
+}
+
+// Close the connection
+mysqli_close($con);
 
 ?>
+
